14babd9114309397b86c47544a588c1830ef3301
Katrin Chirkova <1234_1234_34@mail.ru>
2014-11-04 20:09:39 +0300

    - Simple ToDo with unworked search

--- SimpleToDo/ViewControllers/StartViewController/TDStartViewController.swift
+++ SimpleToDo/ViewControllers/StartViewController/TDStartViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class TDStartViewController: UIViewController, UITableViewDataSource, UIAlertViewDelegate {
+class TDStartViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIAlertViewDelegate, UISearchBarDelegate, UISearchDisplayDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     var notes: [TDNoteEntity] = []
+    var filteredNotes: [TDNoteEntity] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +26,32 @@ class TDStartViewController: UIViewController, UITableViewDataSource, UIAlertVie
 
     // MARK: - table view data sourse
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return notes.count
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            return filteredNotes.count
+        } else {
+            return notes.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: nil)
         
-        cell.textLabel.text = notes[indexPath.row].content + " \(notes[indexPath.row].noteId)"
+        var tempNote : TDNoteEntity
+       
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            tempNote = filteredNotes[indexPath.row]
+        } else {
+            tempNote = notes[indexPath.row]
+        }
+        
+        cell.textLabel.text = tempNote.content
+        
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm"
+        
+        cell.detailTextLabel?.text = "id: \(tempNote.noteId)" + " creation date: \(dateFormatter.stringFromDate(tempNote.creationDate))"
+        
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
         
         return cell
     }
@@ -60,6 +80,47 @@ class TDStartViewController: UIViewController, UITableViewDataSource, UIAlertVie
     func updateData() {
         notes = TDDataModel.sharedInstance.notes
         tableView.reloadData()
+    }
+    
+    // MARK: - filter options
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        filteredNotes = notes.filter({( note: TDNoteEntity) -> Bool in
+            var stringMatch = note.content.rangeOfString(searchText)
+            return stringMatch != nil
+        })
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchString searchString: String!) -> Bool {
+        let scopes = self.searchDisplayController!.searchBar.scopeButtonTitles as [String]
+        let selectedScope = scopes[self.searchDisplayController!.searchBar.selectedScopeButtonIndex] as String
+        self.filterContentForSearchText(searchString, scope: selectedScope)
+        return true
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController!,
+        shouldReloadTableForSearchScope searchOption: Int) -> Bool {
+            let scope = self.searchDisplayController!.searchBar.scopeButtonTitles as [String]
+            self.filterContentForSearchText(self.searchDisplayController!.searchBar.text, scope: scope[searchOption])
+            return true
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.performSegueWithIdentifier("Detail", sender: tableView)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if segue.identifier == "Detail" {
+            let noteDetailViewController = segue.destinationViewController as UIViewController
+            if sender as UITableView == self.searchDisplayController!.searchResultsTableView {
+                let indexPath = self.searchDisplayController!.searchResultsTableView.indexPathForSelectedRow()!
+                let destinationTitle = filteredNotes[indexPath.row].content
+                noteDetailViewController.title = destinationTitle
+            } else {
+                let indexPath = self.tableView.indexPathForSelectedRow()!
+                let destinationTitle = notes[indexPath.row].content
+                noteDetailViewController.title = destinationTitle
+            }
+        }
     }
 
 }
